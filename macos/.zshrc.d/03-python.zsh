685e589c8815ccf72364a01a4806798c591bc50b
Manuel Jesús Romero Mateos <manu.romero.411@gmail.com>
2026-02-28 23:27:59 +0100

    delete files

---
+++ macos/.zshrc.d/03-python.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# ensure "python3" command uses homebrew's version of python3
+alias python3=/opt/homebrew/bin/python3
+
+# OPTIONAL: ensure "python" command uses homebrew's version of python3
+alias python=/opt/homebrew/bin/python3
+
+# pip
+alias pip="/opt/homebrew/bin/python3 -m pip"
+
